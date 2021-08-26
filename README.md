bb4cacc3d8ae6c9f81e4c6f5952f9feaaed0bc85
juancfs <juan.faracco01@gmail.com>
2021-08-25 22:17:47 -0400

    Convert jQuery code to JavaScript code and modify positioning of searcher and footer

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-
 # Covidstats
-Covidstats is a searcher that tracks available data related to the COVID-19 pandemic in the searched country for any valid date. I use [Our World in Data](https://github.com/owid/covid-19-data/tree/master/public/data/) as the COVID data's source.
+Covidstats is a searcher that tracks available data related to the COVID-19 pandemic in the searched country for any valid date. For this purpose, [Our World in Data](https://github.com/owid/covid-19-data/tree/master/public/data/) is used as the COVID data's source.
--- searcher.js
+++ searcher.js
@@ -1,22 +1,19 @@
 let loadISO2File = new Promise((resolve, reject) => {
-    $.getJSON("/JSON/countriesIsoAlpha2.json", (ISO2JSON) => {
-        resolve(ISO2JSON);
-})
-    .fail(() => reject("Failed to load ISO2"));
+    fetch('/JSON/countriesIsoAlpha2.json')
+    .then(response => response.json())
+    .then(ISO2JSON => resolve(ISO2JSON));
 });
 
 let loadISO3File = new Promise((resolve, reject) => {
-    $.getJSON("/JSON/countriesIsoAlpha3.json", (ISO3JSON) => {
-        resolve(ISO3JSON);
-})
-    .fail(() => reject("Failed to load ISO3"));
+    fetch('/JSON/countriesIsoAlpha3.json')
+    .then(response => response.json())
+    .then(ISO3JSON => resolve(ISO3JSON));
 });
 
 let loadCovidFile = new Promise((resolve, reject) => {
-    $.getJSON("https://covid.ourworldindata.org/data/owid-covid-data.json", (covidJSON) => {
-        resolve(covidJSON);
-})
-    .fail(() => reject("Failed to load covid data"));
+    fetch('https://covid.ourworldindata.org/data/owid-covid-data.json')
+    .then(response => response.json())
+    .then(covidJSON => resolve(covidJSON));
 });
 
 let JSONFiles;
@@ -24,51 +21,54 @@ let JSONFiles;
 // Executes this once JSON files are loaded
 Promise.all([loadISO2File, loadISO3File, loadCovidFile]).then(files => {
     JSONFiles = files;
-    $(".loading-box, .shadow").fadeOut();
+    document.querySelector(".loading-box").style.display = "none";
+    document.querySelector(".shadow").style.display = "none";
 
-    $(".search-field").focus();
+    document.querySelector(".search-field").focus();
 
     // Sets max date and default date
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
     const defaultDate = yesterday.getFullYear()+'-'+("0"+(yesterday.getMonth() + 1)).slice(-2)+'-'+
     ("0"+yesterday.getDate()).slice(-2);
-    $("#date-field").attr("max", defaultDate);
-    $("#date-field").val(defaultDate);
+    document.querySelector("#date-field").setAttribute("max", defaultDate);
+    document.querySelector("#date-field").value = defaultDate;
 
-    $(".search-btn").on("click", (() => {
+    document.querySelector(".search-btn").addEventListener("click", (() => {
         checkInput();
     }));
 
-    $(".close-btn").on("click", () => {
-        $(".shadow, .close-btn, .results").fadeOut();
+    document.querySelector(".close-btn").addEventListener("click", () => {
+        document.querySelector(".shadow").style.display = "none";
+        document.querySelector(".close-btn").style.display = "none";
+        document.querySelector(".results").style.display = "none";
     });
 });
 
 // -------- Functions definitions --------
 
-// Function's objective: to check whether or not input is valid
+// Checks whether or not input is valid
 function checkInput() {
-    let country = $(".search-field").val();
+    let country = document.querySelector(".search-field").value;
     country = country.toLowerCase();
     countryCapitalized = country.charAt(0).toUpperCase() + country.slice(1);
     if (JSONFiles[1][country] !== undefined) {
-        $(".input-error").hide();
-        $(".loading-box, .shadow").fadeIn();
+        document.querySelector(".input-error").style.display = "none";
+        document.querySelector(".loading-box").style.display = "inline-block";
+        document.querySelector(".shadow").style.display = "inline-block";
         const countryIso2 = JSONFiles[0][country];
         const countryIso3 = JSONFiles[1][country];
         checkExistence(countryIso2, countryIso3);
     } else {
-        $(".input-error").html(`Oops! It seems that something went wrong.<br>
-        Make sure you are writing the country's name properly.`);
-        $(".input-error").css("display", "inline-block");
+        document.querySelector(".input-error").innerHTML = `Oops! It seems that something went wrong.<br>
+        Make sure you are writing the country's name properly.`;
+        document.querySelector(".input-error").style.display = "inline-block";
     }
 }
 
-// Function's objective: to check if data is available for the provided date
+// Checks if data is available for the provided date
 function checkExistence(countryIso2, countryIso3) {
-    // 
-    const askedDate = $("#date-field").val();
+    const askedDate = document.querySelector("#date-field").value;
     let covidDataContainer = JSONFiles[2][countryIso3]["data"];
     let dataKey;
     let dataExists = false;
@@ -83,56 +83,60 @@ function checkExistence(countryIso2, countryIso3) {
     if (dataExists) {
         generateResults(countryIso2, countryIso3, dataKey);   
     } else {
-        $(".loading-box, .shadow").fadeOut();
-        $(".input-error").html("There's no available data for " + countryCapitalized + " on the required date. <br> Try by entering another date.");
-        $(".input-error").css("display", "inline-block");
+        document.querySelector(".loading-box").style.display = "none";
+        document.querySelector(".shadow").style.display = "none";
+        document.querySelector(".input-error").innerHTML = "There's no available data for " + countryCapitalized + " on the required date. <br> Try by entering another date.";
+        document.querySelector(".input-error").style.display = "inline-block";
     }
 }
 
-// Function's objective: to build the results section.
+// Builds the results section.
 function generateResults(countryIso2, countryIso3, dataKey) {
     // ISO code Alpha 2 is used for fetching the flagLoad. ISO code alpha 3 for fetching country's covid data
         
-    $(".country-covid-th").text(countryCapitalized + "'s COVID-19 data updated to " + dataKey["date"]);
+    document.querySelector(".country-covid-th").innerText = countryCapitalized + "'s COVID-19 data updated to " + dataKey["date"];
 
-    for (let cell of $(".table-content.covid > td")) {
-        if (dataKey[$(cell).attr("class").replaceAll("-", "_")] === undefined) { // Each element's class's used to fetch the data
-            $(cell).text("N/A"); // For not available data
+    for (let cell of document.querySelectorAll(".table-content.covid > td")) {
+        if (dataKey[cell.className.replaceAll("-", "_")] === undefined) { // Each element's class's used to fetch the data
+            cell.innerText = "N/A"; // For not available data
         } else {
-            $(cell).text(dataKey[$(cell).attr("class").replaceAll("-", "_")]);
+            cell.innerText = dataKey[cell.className.replaceAll("-", "_")];
             }
     }
 
         // Fills data in for overall country's data
-    $(".flag").attr("src", "https://flagcdn.com/" + countryIso2.toLowerCase() + ".svg"); // Flag
-    $(".flag").attr("alt", countryCapitalized + "'s flag")
+    document.querySelector(".flag").setAttribute("src", "https://flagcdn.com/" + countryIso2.toLowerCase() + ".svg"); // Flag
+    document.querySelector(".flag").setAttribute("alt", countryCapitalized + "'s flag")
 
-    $(".country-heading").text(countryCapitalized + "'s COVID-19 profile");
-    $(".country-overall-th").text(countryCapitalized + "'s overall data");
+    document.querySelector(".country-heading").innerText = countryCapitalized + "'s COVID-19 profile";
+    document.querySelector(".country-overall-th").innerText = countryCapitalized + "'s overall data";
 
-    for (let cell of $(".table-content.overall > td")) { 
-        if (JSONFiles[2][countryIso3][$(cell).attr("class").replaceAll("-", "_")] === undefined) {
-            $(cell).text("N/A");
+    for (let cell of document.querySelectorAll(".table-content.overall > td")) { 
+        if (JSONFiles[2][countryIso3][cell.className.replaceAll("-", "_")] === undefined) {
+            cell.innerText = "N/A";
         } else {
-                $(cell).text(JSONFiles[2][countryIso3][$(cell).attr("class").replaceAll("-", "_")]);
+                cell.innerText = JSONFiles[2][countryIso3][cell.className.replaceAll("-", "_")];
         }
     }
 
-    $(".iso-code").text(countryIso3); // Ads ISO alpha3 code of the country to the table
+    document.querySelector(".iso-code").innerText = countryIso3; // Ads ISO alpha3 code of the country to the table
 
     let flagLoad = new Promise((resolve) => { // Prevents table from showing before image loads
-        $(".flag").on("load", () => {
+        document.querySelector(".flag").addEventListener("load", () => {
             resolve()
         });
     });
 
     flagLoad.then(() => {
-        $(".loading-box, .shadow").fadeOut();
+        document.querySelector(".loading-box").style.display = "none";
+        document.querySelector(".shadow").style.display = "none";
         showResults();
     });
 
     }
 
 function showResults() {
-    $(".shadow, .close-btn, .results").fadeIn();
+    document.querySelector(".shadow").style.display = "inline-block";
+    document.querySelector(".close-btn").style.display = "inline-block";
+    document.querySelector(".results").style.display = "inline-block";
 }
